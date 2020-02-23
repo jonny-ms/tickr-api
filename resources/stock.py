445886ff8c10052d70f85000da9e251d059fd17d
jonny-ms <j.milne21@gmail.com>
2020-02-23 17:42:46 -0500

    Add Stock resource. Get method makes asynchronous get requests to external financial data api as well as check if stock is being watched is user is logged in.

---
+++ resources/stock.py
@@ -0,0 +1,60 @@
+from flask_restful import Resource
+import config
+import os
+import aiohttp
+import asyncio
+from flask_jwt_extended import (jwt_optional, get_jwt_identity)
+from models.watched import WatchedItemModel
+
+class Stock(Resource):
+  @jwt_optional
+  def get(self, ticker):
+    # make api call:
+    # get realtime quote for header
+    # get intraday data for chart
+    # do I get data for all scopes of chart? ie. intraday for 1 week, historical data up to 5 years?
+    # do I make a db query to see if stock is in watchlist and send boolean as part of response 
+    api_key = os.environ.get('WORLD_TRADING_API_KEY')
+
+    stock_payload = { 
+      'symbol': ticker,
+      'api_token': api_key
+      }
+    intraday_payload = { 
+      'symbol': ticker,
+      'interval': '5',
+      'range': '1',
+      'api_token': api_key
+      }
+
+    async def fetch(session, request):
+      (url, params) = request
+      async with session.get(url, params=params) as response:
+        return await response.json()
+    
+    async def api_call():
+      requests = [
+        ('https://api.worldtradingdata.com/api/v1/stock', stock_payload),
+        ('https://intraday.worldtradingdata.com/api/v1/intraday', intraday_payload)
+        # ('https://api.worldtradingdata.com/api/v1/history', stock_payload)
+        ]
+      tasks = []
+      async with aiohttp.ClientSession() as session:
+        for request in requests:
+          tasks.append(fetch(session, request))
+        responses = await asyncio.gather(*tasks)
+        return responses
+
+    # If logged in, query db to see if stock is being watched
+    watched = False
+
+    if get_jwt_identity():
+      if WatchedItemModel.query.filter_by(user_id=get_jwt_identity(), ticker=ticker).first():
+        watched = True
+    
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    r1, r2 = loop.run_until_complete(api_call())
+
+    return {'watched': watched, 'quote': r1['data'], 'intraday': r2['intraday']}
+
